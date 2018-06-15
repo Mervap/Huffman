b6d648720abc176d65684a29f344995cf3c8463c
Mervap <valeratep@mail.ru>
2018-06-16 01:46:49 +0300

    Revert "Revert "Add modes""
    This reverts commit af807699fdedf065b8871d881ce912d1b69fd320.

--- console/file_decoder.cpp
+++ console/file_decoder.cpp
@@ -14,29 +14,39 @@ file_decoder::file_decoder(std::string filename) : filename(filename),
                                                    dec(bytes) {}
 
 
-void file_decoder::decode_file(std::string to) {
+void file_decoder::decode_file(std::string to, std::string mode) {
     file_reader in(filename, file_size);
     file_writer out(to);
-    size_t i = 1;
-    color_maker color;
-    color.change_mode();
 
-    while (!in.eof()) {
-        encoded_bytes z(in.read_encoded(MAX_READ));
-        out.write_decoded(dec.decode(z));
-
-        ull percents = std::min(100ull, 100ull * i / (file_size > MAX_READ / 8 ? static_cast<ull>(file_size / MAX_READ / 8) : 1));
-
-        if (color.get_mode() == 1 && percents > 33) {
-            color.change_mode();
+    if (mode == "testing") {
+        while (!in.eof()) {
+            encoded_bytes z(in.read_encoded(MAX_READ));
+            out.write_decoded(dec.decode(z));
         }
-        if (color.get_mode() == 2 && percents > 66) {
-            color.change_mode();
-        }
+    } else if (mode == "console") {
+        size_t i = 1;
+        color_maker color;
+        color.change_mode();
 
-        std::cout << "\r" << percents << "%";
-        std::cout.flush();
-        ++i;
+        while (!in.eof()) {
+            encoded_bytes z(in.read_encoded(MAX_READ));
+            out.write_decoded(dec.decode(z));
+
+            ull percents = std::min(100ull, 100ull * i /
+                                            (file_size > MAX_READ / 8 ? static_cast<ull>(file_size / MAX_READ / 8)
+                                                                      : 1));
+
+            if (color.get_mode() == 1 && percents > 33) {
+                color.change_mode();
+            }
+            if (color.get_mode() == 2 && percents > 66) {
+                color.change_mode();
+            }
+
+            std::cout << "\r" << percents << "%";
+            std::cout.flush();
+            ++i;
+        }
+        color.normal_mode();
     }
-    color.normal_mode();
 }
--- console/file_encoder.h
+++ console/file_encoder.h
@@ -15,7 +15,7 @@ struct file_encoder {
     file_encoder() = delete;
 
     explicit file_encoder(std::string filename);
-    void encode_file();
+    void encode_file(std::string mode);
     void write_dictionary();
 
 private:
